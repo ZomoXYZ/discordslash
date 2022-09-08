62b97fc0800c20ce7782ccc45f39dbd9f661df04
zomo <ashleyzomo@gmail.com>
2022-09-08 18:54:06 -0500

    prettier reformatted

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,78 @@
-import { Client, CommandInteraction } from "discord.js";
-import { Command, CommandRunnable } from "./types/commands";
-import { emsg, errorMessage, setEmsgShim } from "./util/errorMessage";
-import { normalizeOption, optionsType } from "./util/normalizeOption";
-import { CommandGenerator } from "./generator/command";
-import { CommandOptionGenerator } from "./generator/option";
-import { registerCommands } from "./register";
+import { Client, CommandInteraction } from 'discord.js';
+import { Command, CommandRunnable } from './types/commands';
+import { emsg, errorMessage, setEmsgShim } from './util/errorMessage';
+import { normalizeOption, optionsType } from './util/normalizeOption';
+import { CommandGenerator } from './generator/command';
+import { CommandOptionGenerator } from './generator/option';
+import { registerCommands } from './register';
 
 export { emsg, errorMessage, setEmsgShim };
 export * from './types/commands';
 
+export { CommandGenerator, CommandOptionGenerator };
+
 const CommandsRaw: Command[] = [],
-    Commands: Map<string, (interaction: CommandInteraction) => void|Promise<void>> = new Map();
-    
+    Commands: Map<
+        string,
+        (interaction: CommandInteraction) => void | Promise<void>
+    > = new Map();
+
 var ClientReady = false,
     ClientToken = '',
     ClientID = '';
 
-export function addCommand(commandRaw: optionsType<CommandRunnable, CommandGenerator>) {
-
+export function addCommand(
+    commandRaw: optionsType<CommandRunnable, CommandGenerator>
+) {
     let commands = normalizeOption(commandRaw),
         commands_n = normalizeOption(commandRaw, 'toJsonRunnable');
 
     CommandsRaw.push(...commands);
-    
-    commands_n.forEach(c => {
+
+    commands_n.forEach((c) => {
         Commands.set(c.name, c.run);
     });
 
     if (ClientReady) {
         registerCommands(CommandsRaw, ClientToken, ClientID);
     }
-
-}
-
-export {
-    CommandGenerator,
-    CommandOptionGenerator
 }
 
 //command execution
 export function initClient(client: Client) {
-    
     //client isn't ready
-    if (client.readyAt !== null)
-        _initClient(client);
-    else
-        client.once('ready', clientReady => _initClient(clientReady));
-
+    if (client.readyAt !== null) _initClient(client);
+    else client.once('ready', (clientReady) => _initClient(clientReady));
 }
 
 function _initClient(client: Client<true>) {
-
     ClientReady = true;
     ClientToken = client.token;
     ClientID = client.user.id;
 
     registerCommands(CommandsRaw, ClientToken, ClientID);
 
-    client.on('interactionCreate', async interaction => {
-
+    client.on('interactionCreate', async (interaction) => {
         if (!interaction.isCommand()) return;
 
         try {
-
             var commandsFound = Commands.get(interaction.commandName);
 
             if (!commandsFound) {
-                throw emsg(`Command ${interaction.commandName} not found`, true, true);
+                throw emsg(
+                    `Command ${interaction.commandName} not found`,
+                    true,
+                    true
+                );
             }
 
             await commandsFound(interaction as CommandInteraction);
-
         } catch (e) {
-
             if (e instanceof errorMessage) {
-
                 if (interaction.deferred || interaction.replied)
                     interaction.editReply(e.content);
-                else
-                    interaction.reply(e);
-                
+                else interaction.reply(e);
             } else console.error(e);
-
         }
     });
-
 }
